8a768ed923388689f7e143ca54ad1a12acaf294f
Lance Wheelock <lwheelock@paypal.com>
2015-07-17 15:34:53 -0700

    fixed distribution & added cross platform support for bs4

--- panobjects.py
+++ panobjects.py
@@ -2,11 +2,10 @@
 
 import os, sys, argparse, getpass
 import pan, re
-from bs4 import BeautifulSoup
+from BeautifulSoup import BeautifulSoup
 
 version = None
 
-''' Import Package as a Requirement '''
 try:
     import pkg_resources
     #version = pkg_resources.require('pan')[0].version
@@ -14,59 +13,82 @@ except ImportError:
     pass
 
 def keygen(device, user, passwd):
-    ''' Generate the API key from the user supplied password '''
     k = pan.api(device, 'keygen', user=user, password=passwd)
-    soup = BeautifulSoup(k.send())
+    soup = BeautifulSoup(k.send(), features="xml")
 
     key = soup.find('key')
-    return key.text
+
+    if key:
+        return key.text
+    else:
+        print "Unable to get key. Check username & password"
+        sys.exit()
+
+def get_object_ip(entry):
+    ''' Return object value attribute for IP '''
+    ipaddr = ""
+    if entry.find('ip-netmask'):
+        ipaddr = entry.find('ip-netmask')
+    elif entry.find('ip-range'):
+        ipaddr = entry.find('ip-range')
+
+    return ipaddr
 
 def parse(soup, search_query):
-    ''' Parse returned XML for objects matching the query string '''
     results = []
 
     if search_query:
         entries = soup.findAll('entry')
         for entry in entries:
-            entry = entry['name']
-            if re.search(search_query, entry, re.IGNORECASE):
-                results.append(entry)
-            else:
-                results = soup
+            ipaddr = get_object_ip(entry)
+            ob_entry = entry['name']
+            if re.search(search_query, ob_entry, re.IGNORECASE):
+                try:
+                    results.append((ob_entry.encode('ascii', 'ignore'), ipaddr.text.encode('ascii', 'ignore')))
+                except AttributeError:
+                    print entry
+
+    if len(results) > 0:
+        return results
+    else:
+        xmlfile = open('pano-objects.xml')
+        data = soup.prettify()
+        xmlfile.write(data)
+        mldfile.close()
+
+        print "Output saved to pano-objects.xml"
+        sys.exit()
+
 
 def send_query(device, ob_type, search_query, key):
-    ''' Generate API call to Panorama '''
     xpath = '/config/shared/' + str(ob_type)
     s = pan.api(device, 'config', xpath=xpath, key=key, action='get')
-    soup = BeautifulSoup(s.send())
+    soup = BeautifulSoup(s.send(), features="xml")
 
     return parse(soup, search_query)
 
-def set_addr(entry):
-    obj_data = entry.split()
-    obj_name = obj_data[0]
-    obj_ip = obj_data[1]
-    xpath = "/config/shared/{0}/entry[@name='{1}']&element=<ip-netmask>{2}</ip-netmask>".format(ob_type, obj_name, obj_ip)
-
-    return xpath
-
 def set_object(device, ob_type, ob_file, key):
-    ''' Add objects to Panorama '''
     obj = open(ob_file, 'r')
     obj_list = obj.readlines()
 
-    fields = len(obj_list)
-
-    if fields >= 3 and (ob_type == 'address' or ob_type == 'service'):
-        raise ObjectTypeError:
-
-    for i in range(fields):
+    for i in range(len(obj_list)):
         obj_list[i] = obj_list[i].rstrip()
 
     for entry in obj_list:
-        xpath = set_addr(entry)
+        obj_data = entry.split()
+        obj_name = obj_data[0]
+        obj_ip = obj_data[1]
+        xpath = "/config/shared/{0}/entry[@name='{1}']&element=<ip-netmask>{2}</ip-netmask>".format(ob_type, obj_name, obj_ip)
+
         s = pan.api(device, 'config', action='set', key=key, xpath=xpath)
+
         s.send()
+
+def printObjects(data):
+    try:
+        print "{0} {1}".format(data[0],data[1])
+    except TypeError:
+        print "Invalid tuple"
 
 def main(argv, version):
     user = getpass.getuser()
@@ -96,13 +118,14 @@ def main(argv, version):
 
     key = keygen(args.device, user, passwd)
 
-    if args.set != None:
-        set_object(args.device, args.type, args.set, key)
-
     if args.query != None:
         s = send_query(args.device, args.type, args.query, key)
+
         for item in s:
-            print item
+            printObjects(item)
+
+    if args.set != None:
+        set_object(args.device, args.type, args.set, key)
 
 if __name__ == "__main__":
     main(sys.argv[1:], version)
--- setup.py
+++ setup.py
@@ -7,30 +7,16 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = ''
-with open('pan/__init__.py', 'r') as fd:
-    version = re.search(r'^__version__\s*=\s*[\'"](^\'"]*)[\'"]',
-                        fd.read(). re.MULTILINE).group(1)
-
-if not version:
-    raise RuntimeError('Version information not available')
-
-with open('README.md', 'r', 'utf-8') as f:
-    readme = f.read()
-with open('HISTORY.md', 'r', 'utf-8') as f:
-    history = f.read()
-
 setup(
     name='pan',
-    version=version,
-    description='Palo Alto Networks API handler'
-    long_description=readme + '\n\n' + history,
-    author='Lance Wheelock'
-    author_email='lawheelock@gmail.com'
-    license='Apache 2.0'
-    packages['pan']
-    install_requires[
-        'bs4 >== 3.2.1',
-        'requests >== 2.3.0'
-    ]
+    version='0.0.1',
+    description='Palo Alto Networks API handler',
+    author='Lance Wheelock',
+    author_email='lawheelock@gmail.com',
+    license='Apache 2.0',
+    packages=['pan'],
+    install_requires=[
+        'BeautifulSoup',
+        'requests',
+    ],
 )
